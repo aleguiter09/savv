89d5705127812b8218ad81e8de4859ef2e921097
aleguiter09 <aleguiter9@gmail.com>
2023-11-21 15:53:25 -0300

    Refactor modal client state

--- src/components/MovementsModal/MovementsModalClient.js
+++ src/components/MovementsModal/MovementsModalClient.js
@@ -28,46 +28,57 @@ export default function MovementsModalClient({
   const [movement, setMovement] = useState({
     category: null,
     amount: 0,
-    type: 0,
+    type: "expense",
     paid_with: "cash",
     comment: "",
     done_at: new Date(),
   });
-  const [category, setCategory] = useState(null);
-  const [date, setDate] = useState(new Date());
-  const [amount, setAmount] = useState(0);
-  const [type, setType] = useState(0);
-  const [paidWith, setPaidWith] = useState("cash");
-  const [comment, setComment] = useState("");
 
   const handleConfirm = async () => {
-    if (category && amount && paidWith && comment && date) {
-      try {
-        const newMovement = {
-          category,
-          amount,
-          type: type === 0 ? "expense" : "income",
-          paid_with: paidWith,
-          comment,
-          done_at: date.toISOString(),
-        };
-        await insertMovement(supabase, newMovement);
-        await upsertBalanceByMonthYear(
-          supabase,
-          date.getMonth(),
-          date.getFullYear(),
-          amount,
-          type,
-        );
-      } catch (err) {
-        console.error(err);
-      } finally {
-        router.push(`/?year=${currentPage.year}&month=${currentPage.month}`);
-      }
-    } else {
-      console.error("Error: Fill out all fields to register");
+    try {
+      await insertMovement(supabase, movement);
+      await upsertBalanceByMonthYear(
+        supabase,
+        movement.done_at.getMonth(),
+        movement.done_at.getFullYear(),
+        movement.amount,
+        movement.type,
+      );
+    } catch (err) {
+      console.error(err);
+    } finally {
       router.push(`/?year=${currentPage.year}&month=${currentPage.month}`);
     }
+  };
+
+  const handleChange = ({ field, value }) => {
+    switch (field) {
+      case "done_at":
+        setMovement((prevData) => ({
+          ...prevData,
+          [field]: value.toISOString(),
+        }));
+        break;
+      case "type":
+        setMovement((prevData) => ({
+          ...prevData,
+          [field]: value === 0 ? "expense" : "income",
+        }));
+        break;
+      default:
+        setMovement((prevData) => ({ ...prevData, [field]: value }));
+        break;
+    }
+  };
+
+  const disableConfirm = () => {
+    return (
+      !movement.category ||
+      !movement.amount ||
+      !movement.comment ||
+      !movement.done_at ||
+      !movement.paid_with
+    );
   };
 
   return (
@@ -77,9 +88,12 @@ export default function MovementsModalClient({
           <DatePicker
             className="mb-3"
             value={date}
-            onValueChange={(d) => setDate(d)}
+            onValueChange={(d) => handleChange("date", d)}
           />
-          <TabGroup className="mb-3" onIndexChange={(value) => setType(value)}>
+          <TabGroup
+            className="mb-3"
+            onIndexChange={(i) => handleChange("type", i)}
+          >
             <TabList variant="solid" className="mb-2 w-full">
               <Tab className="w-full place-content-center">Expenses</Tab>
               <Tab className="w-full place-content-center">Incomes</Tab>
@@ -94,14 +108,14 @@ export default function MovementsModalClient({
                           className={`bg-${
                             cat.color
                           } mx-auto rounded-full p-1.5 ${
-                            cat.id === category
+                            cat.id === movement.category
                               ? "border-2 border-slate-600"
                               : ""
                           }`}
                           path={CATEGORY_ICONS[cat.icon]}
                           size={"35px"}
                           color="white"
-                          onClick={() => setCategory(cat.id)}
+                          onClick={() => handleChange("category", cat.id)}
                         />
                         <p className="m-0 mt-2 truncate text-xs">{cat.title}</p>
                       </div>
@@ -118,14 +132,14 @@ export default function MovementsModalClient({
                           className={`bg-${
                             cat.color
                           } mx-auto rounded-full p-1.5 ${
-                            cat.id === category
+                            cat.id === movement.category
                               ? "border-2 border-slate-600"
                               : ""
                           }`}
                           path={CATEGORY_ICONS[cat.icon]}
                           size={"40px"}
                           color="white"
-                          onClick={() => setCategory(cat.id)}
+                          onClick={() => handleChange("category", cat.id)}
                         />
                         <p className="m-0 mt-2 truncate text-xs">{cat.title}</p>
                       </div>
@@ -141,21 +155,21 @@ export default function MovementsModalClient({
             placeholder="Amount..."
             step="10"
             min="0"
-            onValueChange={(a) => setAmount(a)}
+            onValueChange={(a) => handleChange("amount", a)}
           />
           <TextInput
             placeholder="Comment..."
             className="mb-3"
             value={comment}
-            onChange={(e) => setComment(e.target.value)}
+            onChange={(e) => handleChange("comment", e.target.value)}
           />
           <div className="mx-1 flex flex-row gap-2">
             <div className="flex w-full items-center">
               <input
                 type="radio"
                 value="cash"
-                checked={paidWith === "cash"}
-                onChange={() => setPaidWith("cash")}
+                checked={movement.paid_with === "cash"}
+                onChange={() => handleChange("paid_with", "cash")}
                 className="h-4 w-4 bg-gray-100 text-blue-600  focus:ring-blue-500"
               />
               <label for="cash" className="ml-2 text-sm font-medium text-black">
@@ -166,8 +180,8 @@ export default function MovementsModalClient({
               <input
                 type="radio"
                 value="credit"
-                checked={paidWith === "credit"}
-                onChange={() => setPaidWith("credit")}
+                checked={movement.paid_with === "credit"}
+                onChange={() => handleChange("paid_with", "credit")}
                 className="h-4 w-4 border-gray-300 bg-gray-100 text-blue-600 focus:ring-blue-500"
               />
               <label
@@ -188,7 +202,7 @@ export default function MovementsModalClient({
             <button
               className="w-full rounded-md bg-blue-600 py-2 text-sm font-semibold text-white disabled:opacity-60"
               onClick={handleConfirm}
-              disabled={!category || !amount || !paidWith || !comment || !date}
+              disabled={disableConfirm()}
             >
               Confirm
             </button>
