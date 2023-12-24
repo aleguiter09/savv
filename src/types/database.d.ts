e5f3c0f9e67d392c6e9cdf13769c5c50034de30e
Alejandro Guiter <aguitier@izertis.com>
2023-12-24 18:52:35 +0100

    Inital changes on Database types

---
+++ src/types/database.d.ts
@@ -0,0 +1,29 @@
+export type Movement = {
+  id: number;
+  description: string;
+  amount: number;
+  type: string;
+  category: string;
+};
+
+export type Category = {
+  id: number;
+  title: string;
+  icon: string;
+  color:
+    | "red"
+    | "blue"
+    | "green"
+    | "purple"
+    | "orange"
+    | "cyan"
+    | "pink"
+    | "yellow";
+  for: "income" | "expense";
+};
+
+export type BalanceByDate = {
+  total_expenses: number;
+  total_incomes: number;
+  current_total: number;
+};
