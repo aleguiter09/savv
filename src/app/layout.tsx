3ec5f30f60a61237f558feb1681606d9aa64e7a0
aguiter-inditex <alejandrogui@ext.inditex.com>
2024-06-04 12:19:41 +0200

    Added Top Loader when navigating

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import { createClient } from "@/utils/supabase-server";
 import Navbar from "@/components/Navbar/Navbar";
+import NextTopLoader from "nextjs-toploader";
 export const dynamic = "force-dynamic";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -33,6 +34,15 @@ export default async function RootLayout({
       <body
         className={`pt-4 pb-12 ${inter.className} antialiased bg-gray-100 text-gray-900`}
       >
+        <NextTopLoader
+          color="#3B82F6"
+          initialPosition={0.1}
+          crawlSpeed={200}
+          height={5}
+          easing="ease"
+          speed={200}
+          showSpinner={false}
+        />
         <main className="mx-6 sm:w-[32rem] sm:mx-auto">{children}</main>
         {user && <Navbar />}
       </body>
